ad54167dd30bc01cc98d750417e1a69de51d4acc
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-28 21:27:48 -0700

    Create delete endpoint

--- server/server.js
+++ server/server.js
@@ -53,12 +53,13 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-// delete request for students
-app.delete("/api/students/:studentId", async (req, res) => {
+// delete request for contacts
+app.delete("/api/contacts/:contactId", async (req, res) => {
   try {
-    const studentId = req.params.studentId;
-    await db.query("DELETE FROM students WHERE id=$1", [studentId]);
-    console.log("From the delete request-url", studentId);
+    console.log(req.params);
+    const contactId = req.params.contactId;
+    await db.query("DELETE FROM contacts WHERE contact_id=$1", [contactId]);
+    console.log("From the delete request-url", contactId);
     res.status(200).end();
   } catch (e) {
     console.log(e);
