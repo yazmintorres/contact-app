aff3c09b12b28297b7c248a95f34697d4ba98472
Yazmin <yazminmoniquetorres@gmail.com>
2023-03-28 21:20:55 -0700

    Rename form and fix post request

---
+++ client/src/components/CreateContact.jsx
@@ -0,0 +1,176 @@
+import React, { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+
+const CreateContact = ({
+  onSaveContact,
+  editingStudent,
+  onUpdateStudent,
+  editingContact,
+}) => {
+  // This is the original State with not initial student
+  const [student, setStudent] = useState(
+    editingStudent || {
+      firstname: "",
+      lastname: "",
+      is_current: false,
+    }
+  );
+
+  // create initial state for contacts list
+
+  const [contact, setContact] = useState(
+    editingContact || {
+      firstname: "",
+      lastname: "",
+      phone: "",
+      email: "",
+      notes: "",
+    }
+  );
+
+  //create functions that handle the event of the user typing into the form
+  const handleChange = (property) => {
+    return (e) => {
+      setContact({ ...contact, [property]: e.target.value });
+      console.log(contact);
+    };
+  };
+
+  const clearForm = () => {
+    setContact({
+      firstname: "",
+      lastname: "",
+      phone: "",
+      email: "",
+      notes: "",
+    });
+  };
+
+  //A function to handle the post request
+  const postContact = (contact) => {
+    return fetch("http://localhost:8080/api/contact", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(contact),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log("From the post ", data);
+        //I'm sending data to the List of Students (the parent) for updating the list
+        onSaveContact(data);
+        //this line just for cleaning the form
+        clearForm();
+      });
+  };
+
+  // useEffect(() => {
+  //   console.log(postContact());
+  // });
+
+  //A function to handle the post request
+  const putStudent = (toEditStudent) => {
+    return fetch(`http://localhost:8080/api/students/${toEditStudent.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(toEditStudent),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        onUpdate(data);
+        //this line just for cleaning the form
+        clearForm();
+      });
+  };
+
+  //A function to handle the submit in both cases - Post and Put request!
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const temp = { ...contact };
+    for (let property in temp) {
+      if (!temp[property]) {
+        temp[property] = null;
+      }
+    }
+    console.log("Actual contact variable in this moment", contact);
+    console.log("Temp variable with null replacing", temp);
+    if (contact.id) {
+      putStudent(student);
+    } else {
+      postContact(temp);
+    }
+  };
+
+  return (
+    <Form className="form-students" onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label htmlFor="add-user-name">First Name</Form.Label>
+        <input
+          type="text"
+          id="add-user-name"
+          placeholder="First Name"
+          required
+          value={contact.firstname}
+          onChange={handleChange("firstname")}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor="add-user-lastname">Last Name</Form.Label>
+        <input
+          type="text"
+          id="add-user-lastname"
+          placeholder="Last Name"
+          required
+          value={contact.lastname}
+          onChange={handleChange("lastname")}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor="add-user-phone">Phone Number</Form.Label>
+        <input
+          type="text"
+          id="add-user-phone"
+          placeholder="Phone Number"
+          required
+          value={contact.phone}
+          onChange={handleChange("phone")}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor="add-user-email">Email</Form.Label>
+        <input
+          type="text"
+          id="add-user-email"
+          placeholder="Email"
+          required
+          value={contact.email}
+          onChange={handleChange("email")}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label htmlFor="add-user-notes">Notes</Form.Label>
+        <textarea
+          id="add-user-notes"
+          placeholder="Notes"
+          value={contact.notes}
+          onChange={handleChange("notes")}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Button type="submit" variant="outline-success">
+          {contact.contact_id ? "Edit Contact" : "Add Contact"}
+        </Button>
+        {contact.id ? (
+          <Button type="button" variant="outline-warning" onClick={clearForm}>
+            Cancel
+          </Button>
+        ) : null}
+      </Form.Group>
+    </Form>
+  );
+};
+
+export default CreateContact;
